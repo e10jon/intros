faa71bbc8b30d73db81ae55c4cc4f7592eabbe92
Ethan Czahor <e10jon@users.noreply.github.com>
2024-07-28 08:06:45 -0700

    container

--- web/app/api/route.ts
+++ web/app/api/route.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
+import { Container } from "../../container";
 
 export type Data = { hello: string };
 
-export async function GET(): Promise<NextResponse<Data>> {
-  const data = { hello: "world" };
+export async function GET(request: Request): Promise<NextResponse<Data>> {
+  const cnt = await Container.init(request);
 
-  return NextResponse.json(data);
+  await cnt.sendEmailToUser();
+
+  return NextResponse.json({ hello: "world" });
 }
--- web/container/index.ts
+++ web/container/index.ts
@@ -1,1 +1,28 @@
-export class Container {}
+import { Prisma, PrismaClient } from "@prisma/client";
+
+export class Container {
+  private constructor(private request: Request) {}
+
+  static async init(request: Request) {
+    const cnt = new Container(request);
+    return cnt;
+  }
+
+  private prismaExtension = Prisma.defineExtension({
+    query: {
+      user: {
+        async create({ query, args }) {
+          const result = await query(args);
+          await this.sendEmailToUser(); // can access container methods
+          return result;
+        },
+      },
+    },
+  });
+
+  prisma = new PrismaClient().$extends(this.prismaExtension);
+
+  sendEmailToUser = async () => {
+    console.log("Sending email to user");
+  };
+}
