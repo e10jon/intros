f3a45c09033eb22b7efb8d9bb1f3ac696c41f843
Ethan Czahor <e10jon@users.noreply.github.com>
2024-07-30 07:17:55 -0700

    bullmq events

--- apps/web/bullmq/worker.ts
+++ apps/web/bullmq/worker.ts
@@ -2,12 +2,47 @@ import { Worker } from "bullmq";
 import { connection } from "./connection";
 import { defaultQueueName } from "./queue";
 
-console.log(`Worker is working "${defaultQueueName}"`);
-
 const worker = new Worker(
   defaultQueueName,
   async (job) => {
-    console.log(job.data);
+    // work
+    console.log(
+      `Worker completed job "${job.name}" with args ${JSON.stringify(
+        job.data
+      )}"`
+    );
+    return true;
   },
   { connection }
 );
+
+worker.on("ready", () => {
+  console.log(`Worker is working "${defaultQueueName}"`);
+});
+
+worker.on("active", (job) => {
+  console.log(
+    `Worker received job "${job.name}" with args ${JSON.stringify(job.data)}"`
+  );
+});
+
+worker.on("closing", () => {
+  console.log("Worker is closing");
+});
+
+worker.on("closed", () => {
+  console.log("Worker has closed");
+});
+
+worker.on("failed", (job, err) => {
+  console.log(`Job failed with error ${err.message}`);
+});
+
+worker.on("error", (error) => {
+  console.error(`Worker error: ${error.message}`);
+});
+
+// do this in the worker function itself:
+// worker.on("completed", (job, result) => {
+//   `Worker completed job "${job.name}" with args ${JSON.stringify(job.data)}"`;
+// });
