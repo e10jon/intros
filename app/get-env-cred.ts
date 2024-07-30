0484e5c0f04b3618557f173bfedae1cc1742e67c
Ethan Czahor <e10jon@users.noreply.github.com>
2024-07-29 19:59:20 -0700

    type fix

--- app/get-env-cred.ts
+++ app/get-env-cred.ts
@@ -2,7 +2,8 @@ const env = {
   expoPublicClerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
 };
 
-export const getEnvCred = (key: keyof typeof env) => {
-  if (!env[key]) throw new Error(`Missing environment variable: ${key}`);
-  return env[key];
+export const getEnvCred = (key: keyof typeof env): string => {
+  const val = env[key];
+  if (!val) throw new Error(`Missing environment variable: ${key}`);
+  return val;
 };
