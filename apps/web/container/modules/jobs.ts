799c8501010036debabacbc0b10af45655ea3d61
Ethan Czahor <e10jon@users.noreply.github.com>
2024-08-04 11:42:47 -0700

    recreate cron jobs

--- apps/web/container/modules/jobs.ts
+++ apps/web/container/modules/jobs.ts
@@ -45,4 +45,33 @@ export class JobsModule {
   async sendEmails() {
     // runs every minute on user's sendEmailsTime
   }
+
+  async recreateCronJobs() {
+    // first, delete all repeatable jobs
+    const keys = (await this.cnt.queue.getRepeatableJobs()).map((j) => j.key);
+    console.log(`Deleting repeatable jobs: ${keys.join(" | ")}`);
+    await Promise.all(keys.map((k) => this.cnt.queue.removeRepeatableByKey(k)));
+
+    // now create them
+    await Promise.all([
+      this.cnt.addJob("addMonthlyTokens", {
+        jobOptions: {
+          repeat: { every: 1000 * 60 * 60 * 24 }, // daily
+          attempts: 1,
+        },
+      }),
+      this.cnt.addJob("resetProfilesNumNewIntrosRemaining", {
+        jobOptions: {
+          repeat: { every: 1000 * 60 }, // every minute
+          attempts: 1,
+        },
+      }),
+      this.cnt.addJob("sendEmails", {
+        jobOptions: {
+          repeat: { every: 1000 * 60 }, // every minute
+          attempts: 1,
+        },
+      }),
+    ]);
+  }
 }
