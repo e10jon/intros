b6887ce95b293130e3a15a86a041650c936e5047
Ethan Czahor <e10jon@users.noreply.github.com>
2024-08-05 19:00:24 -0700

    reviewUserReport

--- apps/web/container/modules/ai.ts
+++ apps/web/container/modules/ai.ts
@@ -34,4 +34,40 @@ export class AiModule {
     const response = await this.openAiApiClient.moderations.create({ input });
     return response.results[0];
   }
+
+  async reviewUserReport({
+    profile,
+    messages,
+    report,
+  }: {
+    report: { reason: string };
+    profile: { title: string; bio: string };
+    messages: { body: string }[];
+  }) {
+    const response = await this.openAiApiClient.chat.completions.create({
+      model: "gpt-4o-mini",
+      response_format: { type: "json_object" },
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful moderator designed to review a report that one user made against another user. I will send you the reason for the report, as well as the suspect's profile title and bio, as well as an array of recent messages sent. You will tell me if the report is valid in JSON format { valid: boolean; notes: string }",
+        },
+        {
+          role: "user",
+          content: JSON.stringify({ report, profile, messages }),
+        },
+      ],
+    });
+    if (!response.choices[0].message.content) return [];
+
+    const { valid, notes } = JSON.parse(
+      response.choices[0].message.content
+    ) as {
+      valid: boolean;
+      notes: string;
+    };
+
+    return { valid, notes };
+  }
 }
