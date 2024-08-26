4d0fa2756ad1c8a12a8b064916279621242b8859
Ethan Czahor <ethan@ethanjon.net>
2024-08-26 07:16:11 -0700

    current user

--- packages/container/src/container.ts
+++ packages/container/src/container.ts
@@ -3,6 +3,7 @@ import { prismaExtended } from "./prisma-extended.js";
 import { type LoaderFunctionArgs } from "@remix-run/node";
 import { getAuth } from "@clerk/remix/ssr.server";
 import type { AuthObject } from "@clerk/remix/api.server";
+import type { User } from "@prisma/client";
 
 export class Container {
   private constructor(
@@ -18,4 +19,24 @@ export class Container {
 
   prisma = prismaExtended.call(this);
   queue = createQueue();
+
+  private _currentUser?: Promise<User | null>;
+
+  get getCurrentUser() {
+    if (typeof this._currentUser === "undefined") {
+      this._currentUser =
+        !this.authObject || !this.authObject.userId
+          ? Promise.resolve(null)
+          : this.prisma.user.findUnique({
+              where: { clerkId: this.authObject.userId },
+            });
+    }
+    return this._currentUser;
+  }
+
+  getCurrentUserOrThrow = async () => {
+    const user = await this.getCurrentUser;
+    if (!user) throw new Error("No current user found!");
+    return user;
+  };
 }
